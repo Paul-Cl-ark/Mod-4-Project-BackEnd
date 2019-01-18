985bddcf67bad527d5a1ff01b9760de100696b05
Paul-Cl-ark <clark.paul123@gmail.com>
2019-01-18 23:22:33 +0000

    full crud for to_dos

--- app/controllers/api/v1/to_dos_controller.rb
+++ app/controllers/api/v1/to_dos_controller.rb
@@ -1,5 +1,4 @@
 class Api::V1::ToDosController < ApplicationController
-  before_action :find_to_do, only: [:update, :destroy]
 
   def index
     # user = User.find(params[:user_id])
@@ -9,16 +8,17 @@ class Api::V1::ToDosController < ApplicationController
   end
 
   def create
-    to_do = ToDo.create(header: to_do_params[:header], description: to_do_params[:description], user_id: to_do_params[:user_id])
+    to_do = ToDo.create(header: params[:header], description: params[:description])
     if to_do.save
-      render json: to_do, status: :accepted
+      render json: to_do
     else
       render json: { errors: to_do.errors.full_messages }, status: :unprocessible_entity
     end
   end
 
   def update
-    to_do.update(header: to_do_params[:header], description: to_do_params[:description], user_id: to_do_params[:user_id])
+    to_do = ToDo.find(params[:id])
+    to_do.update(header: params[:header], description: params[:description])
     if to_do.save
       render json: to_do, status: :accepted
     else
@@ -27,16 +27,8 @@ class Api::V1::ToDosController < ApplicationController
   end
 
   def destroy
-    to_do.destroy
-  end
-
-  private
-
-  def to_do_params
-    params.permit(:header, :description, :user_id)
-  end
-
-  def find_to_do
     to_do = ToDo.find(params[:id])
+    to_do.destroy()
   end
+
 end
--- app/models/to_do.rb
+++ app/models/to_do.rb
@@ -1,3 +1,3 @@
 class ToDo < ApplicationRecord
-  belongs_to :user
+  # belongs_to :user
 end
