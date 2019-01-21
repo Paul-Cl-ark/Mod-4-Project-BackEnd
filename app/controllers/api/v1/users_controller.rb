691c7860025215f14b075ae194ed2b986347da3c
Paul-Cl-ark <clark.paul123@gmail.com>
2019-01-21 13:44:09 +0000

    prep for login form, current user needs to be edited before implementation

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,26 +7,27 @@ class Api::V1::UsersController < ApplicationController
       @token = encode_token(user_id: @user.id)
       render json: { user: @user, jwt: @token }, status: :created
     else
-      render json: { error: 'failed to create user' }, status: :not_acceptable
+      render json: { error: 'Failed to create user' }, status: :not_acceptable
+    end
+  end
+
+  def login
+    @user = User.find_by(email: user_params[:email])
+    if @user && @user.authenticate(user_params[:password])
+      @token = encode_token(user_id: @user.id)
+      render json: { user: @user, jwt: @token }, status: :accepted
+    else
+      render json:  { error: 'Access DENIED'}, status: :unauthorized
     end
   end
 
   # def update
-  #   user.update(user_params)
-  #   if user.save
-  #     render json: user, status: :accepted
-  #   else
-  #     render json: { errors: user.errors.full_messages }, status: :unprocessible_entity
-  #   end
   # end
 
   private
 
   def user_params
-    params.permit(:first_name, :last_name, :email, :password)
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
   end
-  #
-  # def find_user
-  #   @user = User.find_by(user_params)
-  # end
+
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,7 +31,8 @@ class ApplicationController < ActionController::API
   end
 
   def logged_in?
-    !!current_user
+    # !!current_user
+    true
   end
 
   def authorized
